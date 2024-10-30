07072dd95ac4eb78dee7d676bf5263d372431711
Rafael Ricardo <143666698+DEVRAFA01@users.noreply.github.com>
2024-10-30 20:51:35 -0300

    Delete dogplay directory

--- dogplay/Repository/RepositoryTutor.java
+++
@@ -1,6 +0,0 @@
-package br.com.dogplay.Repository;
-
-import br.com.dogplay.models.Tutor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryTutor extends JpaRepository<Tutor, Long> {}
