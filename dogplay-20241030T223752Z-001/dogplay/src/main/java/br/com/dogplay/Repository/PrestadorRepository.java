c9c7b0f2beeb450b46b9fecefe9f58ef6f5b4a2f
Rafael Ricardo Cabral da Silva Cunha <143666698+DEVRAFA01@users.noreply.github.com>
2024-11-03 19:22:54 -0300

    Delete dogplay-20241030T223752Z-001/dogplay directory

--- dogplay-20241030T223752Z-001/dogplay/src/main/java/br/com/dogplay/Repository/PrestadorRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.dogplay.Repository;
-
-import br.com.dogplay.models.Prestador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrestadorRepository extends JpaRepository<Prestador, Long> {
-
-}
--- dogplay-20241030T223752Z-001/dogplay/src/main/java/br/com/dogplay/models/Pet/PetResponse.java
+++
@@ -1,8 +0,0 @@
-package br.com.dogplay.models.Pet;
-
-public record PetResponse(String nome, int idade, String nomeTutor) {
-    public PetResponse(Pet pet, int idade) {
-        this(pet.getNome(), idade, pet.getTutor().getNome());
-
-    }
-}
